8e3ccc0941cc6a8cfbc474f5248657f0d3c784e8
Трусов Сергей <tserg@hub.sknt.ru>
2023-12-30 19:10:23 +0300

    Codes: добавлено поле PREFIX_LOCK & новые коды локальных ошибок

--- src/Codes.php
+++ src/Codes.php
@@ -3,6 +3,8 @@
 namespace MemSess;
 
 class Codes {
+    public const PREFIX_LOCK = '.lock';
+
     public const OK = 1;
     public const E_WRONG_COMMAND = 2;
     public const E_WRONG_PARAMS = 3;
@@ -14,11 +16,15 @@ class Codes {
     public const E_RECORD_BEEN_CHANGED = 9;
     public const E_LIMIT_PER_SEC_EXCEEDED = 10;
     public const E_DUPLICATE_SESSION = 11;
-    public const E_UNKNOWN = 12;
-    public const E_SEND = 13;
-    public const E_SAVE_BEFORE_LOAD = 14;
 
     public const E_WRONG_LIFETIME = 1001;
     public const E_WRONG_TIMEOUT = 1002;
     public const E_WRONG_ATTEMPTS = 1003;
+    public const E_WRONG_KEY = 1004;
+    public const E_WRONG_UUID = 1005;
+    public const E_WRONG_LIMIT = 1006;
+
+    public const E_UNKNOWN = 1101;
+    public const E_SEND = 1102;
+    public const E_SAVE_BEFORE_LOAD = 1103;
 };
